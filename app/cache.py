f28791e7cb9e9426e15fa2b45cadeb697074f291
Sebastian885 <96152178+Sebastiann885@users.noreply.github.com>
2025-08-21 17:31:20 -0600

    Update cache.py

--- app/cache.py
+++ app/cache.py
@@ -1,13 +1,9 @@
+import redis.asyncio as redis
 import os
-import redis.asyncio as aioredis
-
-_redis = None  # Singleton para no abrir múltiples conexiones
 
 async def init_redis():
-    global _redis
-    if _redis is None:
-        redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
-        _redis = aioredis.from_url(
-            redis_url, encoding="utf-8", decode_responses=True
-        )
-    return _redis
+    return redis.from_url(
+        os.getenv("REDIS_URL", "redis://localhost:6379/0"),
+        encoding="utf-8",
+        decode_responses=True
+    )
